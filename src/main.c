34ace39c82acaf713580f307c1833da005758118
Thomas Crha <thomas@9bitbyte.com>
2024-09-07 22:33:40 +1000

    disabled json parsing

--- src/main.c
+++ src/main.c
@@ -39,37 +39,36 @@ int main(int argc, char *argv[]) {
             return EXIT_FAILURE;
         }
 
-        char json_message_str[bow_config->buffer_size];
-        if (fgets(json_message_str, bow_config->buffer_size, pipe_fd) == NULL) {
+        char message[bow_config->buffer_size];
+        if (fgets(message, bow_config->buffer_size, pipe_fd) == NULL) {
             bow_log_error("Input is larger than buffer size");
             continue;
         }
         fclose(pipe_fd);
 
         bow_log_info("Received string");
-        bow_log_debug("%s", json_message_str);
+        bow_log_debug("%s", message);
 
         // gpointer message = g_new(struct bow_message, 1);
         // ((struct bow_message *)message)->expression = expression;
         // ((struct data *)data)->window_timeout = window_timeout;
-        struct bow_message message;
-        if (bow_message_parse(json_message_str, &message) != 0) {
-            bow_log_error("Failed to parse message");
-            continue;
+        // struct bow_message message;
+        // if (bow_message_parse(json_message_str, &message) != 0) {
+        //     bow_log_error("Failed to parse message");
+        //     continue;
+        // }
+        //
+        // bow_log_debug("expression: %s", message.expression);
+        // bow_log_debug("type: %s", message.type);
+        // bow_log_debug("config: %s", message.config);
+
+        int code = bow_create_run_window(message, bow_config->window_timeout);
+
+        if (code != 0) {
+            bow_log_error("bow_create_run_window() returned %d", code);
         }
 
-        bow_log_debug("expression: %s", message.expression);
-        bow_log_debug("type: %s", message.type);
-        bow_log_debug("config: %s", message.config);
-
-        //      int code = bow_create_run_window(message, bow_config->window_timeout);
-        //
-        //      if (code != 0) {
-        //          bow_log_error("bow_create_run_window() returned %d", code);
-        // continue;
-        //      }
+        bow_destroy_config(bow_config);
+        return EXIT_SUCCESS;
     }
-
-    bow_destroy_config(bow_config);
-    return EXIT_SUCCESS;
 }
